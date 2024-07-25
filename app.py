7d4b06d4dc8795c2b70b7f051ef896c4a6263d2d
elorisraeli <elorisraeli@gmail.com>
2024-07-25 20:37:17 +0300

    return to specific port

--- app.py
+++ app.py
@@ -1,19 +1,4 @@
 from flask_files import app
-import socket
 
-def find_open_port(start_port, end_port):
-    for port in range(start_port, end_port + 1):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            if s.connect_ex(('0.0.0.0', port)) != 0:
-                return port
-    raise Exception("No available ports found in the specified range.")
-
-start_port = 2577
-end_port = 2599
-
-try:
-    open_port = find_open_port(start_port, end_port)
-    app.run(debug=True, host="0.0.0.0", port=open_port)
-    print(f"Running on port {open_port}")
-except Exception as e:
-    print(str(e))
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=2577)
